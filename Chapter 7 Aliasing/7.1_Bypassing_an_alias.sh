355d115d59e30e0284eaab4979a0717f139168e2
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-09-09 09:02:56 +0100

    Add files via upload

---
+++ Chapter 7 Aliasing/7.1_Bypassing_an_alias.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#alias ls = ´ĺs --color=auto´
+echo 'alias ls = ls --color=auto'
+echo 'Can use builtin command ls'
+echo 'Can use full path to builtin command /bin/ls'
+echo 'Can use \ls or l\s'
+echo 'Can use quotes around ls'
+
+
+
